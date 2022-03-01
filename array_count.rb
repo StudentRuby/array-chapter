a5d70f68ae8017cd5a12025ac4dc62879835d068
samcortez07 <samuelscortez@outlook.com>
2022-03-01 04:14:19 +0000

    Array

--- array_count.rb
+++ array_count.rb
@@ -1,5 +1,5 @@
 # Create an Array and use the count method to print how many elements are in the Array.
-# 
+#
 # Expected output:
 # "Enter at least 2 numbers, separated by spaces:"
 # 2
@@ -9,3 +9,7 @@ p "Enter at least 2 numbers, separated by spaces:"
 user_string = gets.chomp
 
 user_numbers = user_string.split
+
+c = user_numbers.count
+
+p c
--- array_min_max_difference.rb
+++ array_min_max_difference.rb
@@ -1,9 +1,17 @@
 # Create an Array of numbers and output the number with the lowest value in
 # the array, the number with the highest value in the array, and the difference
 # between the highest value and the lowest value.
-# 
+#
 #   lowest_number
 #   highest_number
 #   difference
 
 array = [12, 23, 41, 73, 19, 6]
+
+min = array.min
+max = array.max
+diff = max - min
+
+p min
+p max
+p diff
